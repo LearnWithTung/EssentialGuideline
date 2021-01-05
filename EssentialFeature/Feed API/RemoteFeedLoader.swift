3f08da67a0fb6db2a8e30d3ce3363d9388e71e98
Tung Vu Duc <tungvuduc2805@gmail.com>
2021-01-05 16:48:02 +0700

    Move `RemoteFeedLoader` and `HTTPClient` to production

---
+++ EssentialFeature/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,44 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeature
+//
+//  Created by Tung Vu Duc on 05/01/2021.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
+
+public class RemoteFeedLoader {
+    
+    private let url: URL
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load(completion: @escaping (Error) -> Void = {_ in}) {
+        client.get(from: url) { result in
+            switch result {
+            case .failure:
+                completion(.connectivity)
+            case .success:
+                completion(.invalidData)
+            }
+        }
+    }
+}
