3a53d2193a2bca2085934ae4546a083e8072de34
Tung Vu Duc <tungvuduc2805@gmail.com>
2021-01-05 14:18:34 +0700

    add `FeedItem`

---
+++ EssentialFeature/Feed Feature/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeature
+//
+//  Created by Tung Vu Duc on 05/01/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: Int
+    let email: String
+    let firstName: String
+    let lastName: String
+    let url: String
+}
