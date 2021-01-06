372cd98f4c8e4325466540e79c783bf4a104b2ca
Tung Vu Duc <tungvuduc2805@gmail.com>
2021-01-06 13:09:39 +0700

    compose feed UI and run for the first time !

--- EssentialiOSApp/SceneDelegate.swift
+++ EssentialiOSApp/SceneDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import EssentialFeature
+import EssentialGuidelineiOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +18,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        
+        let url = URL(string: "https://reqres.in/api/users")!
+        let client = URLSessionHTTPClient(session: .shared)
+        let feedLoader = RemoteFeedLoader(url: url, client: client)
+        let feedImageLoader = FeedImageDataLoaderWithSDWebImage()
+        window?.rootViewController = FeedViewController(feedLoader: feedLoader, imageLoader: feedImageLoader)
     }
 
 
