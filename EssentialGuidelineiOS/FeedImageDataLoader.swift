7ce6fe8ee7d316d797917a4097043e055fa2a590
Tung Vu Duc <tungvuduc2805@gmail.com>
2021-01-06 10:58:31 +0700

    add `FeedImageDataLoaderWithSDWebImage` implement `FeedImageDataLoader`  protocol

--- EssentialGuidelineiOS/FeedImageDataLoader.swift
+++ EssentialGuidelineiOS/FeedImageDataLoader.swift
@@ -6,7 +6,25 @@
 //
 
 import Foundation
+import SDWebImage
 
 public protocol FeedImageDataLoader {
     func loadImageData(from url: URL, completion: @escaping (Result<Data, Error>) -> Void)
+}
+
+public class FeedImageDataLoaderWithSDWebImage : FeedImageDataLoader{
+    private let manager = SDWebImageManager()
+    
+    public init() {}
+    
+    public func loadImageData(from url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let manger = SDWebImageManager()
+        manger.loadImage(with: url, options: .continueInBackground, progress: nil) { (_, data, error, _, _, _) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(data!))
+            }
+        }
+    }
 }
