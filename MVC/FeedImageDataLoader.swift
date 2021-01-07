e7224ac7a998cf7b03e28ffe2223c2579fb45fbd
Tung Vu Duc <tungvuduc2805@gmail.com>
2021-01-07 13:29:59 +0700

    Duplicate test code from `EssentialGuidelineTests` to `MVCTests`

--- MVC/FeedImageDataLoader.swift
+++ MVC/FeedImageDataLoader.swift
@@ -6,25 +6,24 @@
 //
 
 import Foundation
-import SDWebImage
 
 public protocol FeedImageDataLoader {
     func loadImageData(from url: URL, completion: @escaping (Result<Data, Error>) -> Void)
 }
 
-public class FeedImageDataLoaderWithSDWebImage : FeedImageDataLoader{
-    private let manager = SDWebImageManager()
-    
-    public init() {}
-    
-    public func loadImageData(from url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
-        let manger = SDWebImageManager()
-        manger.loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, error, _, _, _) in
-            if let error = error {
-                completion(.failure(error))
-            } else {
-                completion(.success(image?.pngData() ?? Data()))
-            }
-        }
-    }
-}
+//public class FeedImageDataLoaderWithSDWebImage : FeedImageDataLoader{
+//    private let manager = SDWebImageManager()
+//    
+//    public init() {}
+//    
+//    public func loadImageData(from url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+//        let manger = SDWebImageManager()
+//        manger.loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, error, _, _, _) in
+//            if let error = error {
+//                completion(.failure(error))
+//            } else {
+//                completion(.success(image?.pngData() ?? Data()))
+//            }
+//        }
+//    }
+//}
