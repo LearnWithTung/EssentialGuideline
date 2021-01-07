382b954244b14ec63ab3eb7cf5025c5b1ed9447d
Tung Vu Duc <tungvuduc2805@gmail.com>
2021-01-07 14:09:47 +0700

    Move `FeedCellController` to its own file

--- MVC/Controllers/FeedViewController.swift
+++ MVC/Controllers/FeedViewController.swift
@@ -8,31 +8,6 @@
 import UIKit
 import EssentialFeature
 
-class FeedCellController {
-    
-    private let imageDataLoader: FeedImageDataLoader
-    private let model: FeedItem
-    
-    init(imageDataLoader: FeedImageDataLoader, model: FeedItem) {
-        self.imageDataLoader = imageDataLoader
-        self.model = model
-    }
-    
-    func view(_ tableView: UITableView) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedUserCell") as! FeedUserCell
-        cell.emailLabel.text = model.email
-        cell.firstNameLabel.text = model.firstName
-        cell.lastNameLabel.text = model.lastName
-        cell.userImageView.image = nil
-        imageDataLoader.loadImageData(from: model.url) { result in
-            let data = try? result.get()
-            cell.userImageView.image = data.map(UIImage.init) ?? nil
-        }
-        return cell
-    }
-    
-}
-
 final public class FeedViewController: UITableViewController {
     
     private var refreshController: FeedRefreshController?
